c93bdda8683e6a3aaf43398346c88b2eedacf9b7
user <user@DESKTOP-ERR5D88>
2023-07-17 17:09:45 +0900

    project1

---
+++ project1/src/main/java/com/odakka/pro1/BoardService.java
@@ -0,0 +1,32 @@
+package com.odakka.pro1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.stereotype.Service;
+
+@Service("boardService")
+public class BoardService {
+
+	@Inject
+	@Named("boardDAO")
+	private BoardDAO boardDAO;
+
+	// 보드 리스트 불러오는 메소드
+	public List<Map<String, Object>> boardlist() {
+		return boardDAO.boardList();
+	}
+
+	public BoardDTO detail(String bno) {
+		
+		return boardDAO.detail(bno);
+	}
+
+
+}
